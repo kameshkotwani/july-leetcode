14d665a1e6ea40b2c090d4b42a2119a28f381c43
Kamesh Kotwani <36355951+kameshkotwani@users.noreply.github.com>
2020-07-13 14:25:45 +0530

    added july-13 solution

---
+++ same-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+  bool identicalTrees(TreeNode* p, TreeNode* q)
+  {
+     if(p==NULL && q==NULL ) return true;
+     if (p!=NULL && q!=NULL) 
+        { 
+            return
+            ( 
+                p->val == q->val && 
+                identicalTrees(p->left, q->left) && 
+                identicalTrees(p->right, q->right) 
+            ); 
+        } 
+    return false;
+  }
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+      bool ans= identicalTrees(p,q);
+      return ans;
+    }
+};
